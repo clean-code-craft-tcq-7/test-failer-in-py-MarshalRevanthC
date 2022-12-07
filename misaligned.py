34d97050baacba4af4dcbee002d9a0c0060363ed
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-12-07 10:13:37 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,17 +1,29 @@
 
-major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-colorCode = {}
-def print_color_map():
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-            colorCode[str({major} | {minor})] = i * 5 + j
-    return len(major_colors) * len(minor_colors)
-result = print_color_map()
-assert(result == 25)
-assert(colorCode[str({"Red"} | {"Orange"})] == 7)
-assert(colorCode[str({"Violet"} | {"Green"})] == 23)
-print("All is well (maybe!)\n")
+from value_lib import *
 
+def final_index():
+    global dict_allingment
+    final_nth_index = dict_allingment[-1]
+    return final_nth_index
+
+def Index_limits():
+    global major_index, minor_index
+    return major_index * 5 + minor_index + 1
+
+def print_allignment(major, minor):
+    global major_index, minor_index
+    print(f'{Index_limits()} | {major} | {minor}')
+    return Index_limits()
+
+def access_color_map():
+    global major_index, minor_index, dict_allingment
+    for major in major_colors:
+        for minor in minor_colors:
+            major_index, minor_index = major_colors.index(major), minor_colors.index(minor)
+            dict_allingment.append(print_allignment(major, minor))
+    return len(major_colors) * len(minor_colors), final_index()
+
+result, final_nth_index_mul = access_color_map()
+assert(result == final_nth_index_mul)
+print("All is well (maybe!)\n")
 
